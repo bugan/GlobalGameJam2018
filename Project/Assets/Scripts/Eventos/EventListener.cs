06828dc35a97b94248ea630343d91dafc690eeca
Ricardo Bugan <dones.bugan@gmail.com>
2018-01-26 11:50:52 -0200

    Adicionando Sistema de eventos

---
+++ Project/Assets/Scripts/Eventos/EventListener.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventListener : MonoBehaviour
+{
+    public EventoCustomizado evento;
+    public UnityEvent reagir;
+
+    void OnEnable()
+    {
+		this.evento.adicionarListener(this);
+    }
+
+    void OnDisable()
+    {
+		this.evento.dispararEvento();
+		this.evento.removerListener(this);
+    }
+}
---
+++ Project/Assets/Scripts/Eventos/EventoCustomizado.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "EventoCustomizado", fileName = "NovoEvento")]
+public class EventoCustomizado : ScriptableObject
+{
+    List<EventListener> clientes;
+
+    void OnEnable()
+    {
+        this.clientes = new List<EventListener>();
+    }
+
+    public void dispararEvento()
+    {
+        for (int i = 0; i < this.clientes.Count; i++)
+        {
+			this.clientes[i].reagir.Invoke();
+        }
+    }
+    public void adicionarListener(EventListener cliente)
+    {
+        this.clientes.Add(cliente);
+    }
+    public void removerListener(EventListener cliente)
+    {
+        this.clientes.Remove(cliente);
+    }
+
+}
